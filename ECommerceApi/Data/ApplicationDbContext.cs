ef9277e9d0082811073d6549a1e0cb2836346978
Wei Zheng <wei3_@hotmail.com>
2025-08-09 21:48:16 +0200

    Add error table

--- ECommerceApi/Data/ApplicationDbContext.cs
+++ ECommerceApi/Data/ApplicationDbContext.cs
@@ -16,10 +16,13 @@ namespace ECommerceApi.Data
 
             base.OnModelCreating(modelBuilder);
         }
+        
         public DbSet<Product> Products { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Error> Errors { get; set; }
+
     }
 }
---
+++ ECommerceApi/Models/Error.cs
@@ -0,0 +1,10 @@
+namespace ECommerceApi.Models
+{
+    public class Error
+    {
+        public Guid Id { get; set; }
+        public required string Message { get; set; }
+        public string? StackTrace { get; set; }
+        public DateTime Date{ get; set; }
+    }
+}
